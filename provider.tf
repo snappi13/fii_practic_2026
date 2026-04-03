034d790f6e50c5ceca547932cc43f59ad266c0e3
chakalak <iustin-alexandru.achitenei@student.tuiasi.ro>
2026-04-03 10:49:43 +0000

    s3 name

--- provider.tf
+++ provider.tf
@@ -17,7 +17,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket  = "oidcstate79874132464"
+    bucket  = "kdsjhfhdsohgiodshg6548"
     key     = "terraform.tfstate"
     region  = "eu-north-1"
     encrypt = true
